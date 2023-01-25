23efc2983cdf56d758da5ed6735c4c8013a0acd9
Your Name <you@example.com>
2023-01-25 10:20:18 -0300

    first commit

---
+++ public/js/api.js
@@ -0,0 +1,4 @@
+let api_key = "c213464caaeebeee17a3c0ac8e7e1bc8"
+let img_url ="https://image.tmdb.org/t/p/w500/"
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
